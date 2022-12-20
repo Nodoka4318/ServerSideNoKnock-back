d170241b730c38fc8441af46cd824ca1306bc96c
Nodoka4318 <78198198+Nodoka4318@users.noreply.github.com>
2022-12-20 18:00:56 +0900

    a

--- src/main/java/ml/heartfulcpvp/noknockback/NoKnockback.java
+++ src/main/java/ml/heartfulcpvp/noknockback/NoKnockback.java
@@ -8,7 +8,11 @@ import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketEvent;
 import ml.heartfulcpvp.noknockback.commands.NoKnockbackCommand;
 import ml.heartfulcpvp.noknockback.playerdata.PlayerData;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerVelocityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
@@ -27,14 +31,35 @@ public class NoKnockback extends JavaPlugin {
 
         var protocol = ProtocolLibrary.getProtocolManager();
 
+        // エンティティ攻撃
         protocol.addPacketListener(new PacketAdapter(this,
                 ListenerPriority.NORMAL,
                 PacketType.Play.Server.ENTITY_VELOCITY) {
             @Override
             public void onPacketSending(PacketEvent e) {
                 if (PlayerData.getPlayerData().isEnabled(e.getPlayer())) {
-                    sendInfoMessage(e.getPlayer(), "Cancelling packet..");
+                    if (e.getPacket().getIntegers().read(0) != e.getPlayer().getEntityId())
+                        return;
+
                     e.setCancelled(true);
+                }
+            }
+        });
+
+        // 爆発
+        protocol.addPacketListener(new PacketAdapter(this,
+                ListenerPriority.NORMAL,
+                PacketType.Play.Server.EXPLOSION) {
+            @Override
+            public void onPacketSending(PacketEvent e) {
+                if (PlayerData.getPlayerData().isEnabled(e.getPlayer())) {
+                    var packet = e.getPacket();
+                    // ref: https://wiki.vg/Protocol#Explosion
+                    packet.getFloat().write(1, 0f);
+                    packet.getFloat().write(2, 0f);
+                    packet.getFloat().write(3, 0f);
+
+                    e.setPacket(packet);
                 }
             }
         });
